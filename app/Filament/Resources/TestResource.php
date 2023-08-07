35e9d090553d2d1c3cf74597cb581bd10c8069a6
mmathewson <mike@corporatecomm.com>
2023-08-07 12:31:12 -0400

    Initial commit

---
+++ app/Filament/Resources/TestResource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TestResource\Pages;
+use App\Models\Test;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class TestResource extends Resource
+{
+    protected static ?string $model = Test::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                SpatieMediaLibraryFileUpload::make('image')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTests::route('/'),
+            'create' => Pages\CreateTest::route('/create'),
+            'edit' => Pages\EditTest::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Models/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Test extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+}
